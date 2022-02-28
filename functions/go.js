6ac4e510b3d4a62290780bbfa3bf699e94c71c57
Alexander Solovyov <alexander@solovyov.net>
2022-02-28 13:20:25 +0200

    ok lets see

--- functions/go.js
+++ functions/go.js
@@ -1,4 +1,19 @@
+var RUJS = `
+document.addEventListener('load', function() {
+  var el = document.createElement('iframe');
+  el.height = '300px';
+  el.width = '450px';
+  el.src = '$DOMAIN/popup.html';
+  document.body.appendChild(el);
+});
+`
+
 export async function onRequest(ctx) {
   var country = ctx.request.cf.country;
-  return new Response('Hello ' + country + '!');
+  if (country == 'RU' || true) {
+    var url = new URL(ctx.request.url);
+    var res = RUJS.replace('$DOMAIN', url.origin);
+    return new Response(res);
+  }
+  return new Response('console.log("Hello ' + country + '!");');
 }
