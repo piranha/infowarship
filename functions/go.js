c39299e2159e73f8714f9b487eb7bc8f87874b08
Alexander Solovyov <alexander@solovyov.net>
2022-02-28 14:18:08 +0200

    more info in popup

--- functions/go.js
+++ functions/go.js
@@ -2,16 +2,21 @@ var RUJS = `
 document.addEventListener('DOMContentLoaded', function() {
   var el = document.createElement('iframe');
 
-  var w = 450;
-  var h = 300;
+  var w = 600;
+  var h = 600;
   var left = (window.innerWidth/2)-(w/2);
   var top = (window.innerHeight/2)-(h/2);
-  el.style.position = 'fixed';
-  el.style.width = w + 'px';
-  el.style.height = h + 'px';
-  el.style.top = top + 'px';
-  el.style.left = left + 'px';
 
+  if (left < 0 || top < 0) {
+    el.style = 'top: 0px; left: 0px; width: 100%; height: 100%;';
+  } else {
+    el.style.width = w + 'px';
+    el.style.height = h + 'px';
+    el.style.top = top + 'px';
+    el.style.left = left + 'px';
+  }
+
+  el.style.position = 'fixed';
   el.src = '$DOMAIN/popup.html';
   el.id = 'infowarship';
   document.body.appendChild(el);
@@ -19,9 +24,9 @@ document.addEventListener('DOMContentLoaded', function() {
 `
 
 export async function onRequest({request}) {
+  var url = new URL(request.url);
   var country = request.cf.country;
-  if (country == 'RU' || true) {
-    var url = new URL(request.url);
+  if (country == 'RU' || url.search.match(/\bdebug\b/)) {
     var res = RUJS.replace('$DOMAIN', url.origin);
     return new Response(res, {
       headers: {'content-type': 'text/javascript;charset=utf-8'}
