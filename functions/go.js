5a1bb6fd3ec5f7238fbe331c1de67256879f25ff
nrn <nyancache@gmail.com>
2022-03-01 13:47:10 +0200

    redesign

--- functions/go.js
+++ functions/go.js
@@ -8,14 +8,8 @@ function main() {
   var left = (window.innerWidth/2)-(w/2);
   var top = (window.innerHeight/2)-(h/2);
 
-  if (left < 0 || top < 0) {
-    el.style = 'top: 0px; left: 0px; width: 100%; height: 100%;';
-  } else {
-    el.style.width = w + 'px';
-    el.style.height = h + 'px';
-    el.style.top = top + 'px';
-    el.style.left = left + 'px';
-  }
+  el.style = 'top: 0; left: 0; width: 100vw; height: 100vh; bottom: 0; right: 0; z-index: 2000000;';
+
 
   el.style.position = 'fixed';
   el.src = '$DOMAIN/popup.html';
